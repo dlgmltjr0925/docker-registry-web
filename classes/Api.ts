fe81f3f3592f1527b1e5a7e99c9cc6d2d61c9b51
fine <dlgmltjr0925@gmail.com>
2021-01-21 08:49:59 +0900

    API 클래스 구현

---
+++ classes/Api.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { response404, response500 } from '../utils/api';
+
+export default class Api {
+  constructor() {
+    this.get = this.get.bind(this);
+    this.post = this.post.bind(this);
+    this.put = this.put.bind(this);
+    this.delete = this.delete.bind(this);
+    this.handler = this.handler.bind(this);
+  }
+
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    response404(res);
+  }
+
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    response404(res);
+  }
+
+  async put(req: NextApiRequest, res: NextApiResponse) {
+    response404(res);
+  }
+
+  async delete(req: NextApiRequest, res: NextApiResponse) {
+    response404(res);
+  }
+
+  async handler(req: NextApiRequest, res: NextApiResponse) {
+    debugger;
+    console.log(req);
+    try {
+      switch (req.method) {
+        case 'GET':
+          await this.get(req, res);
+          break;
+        case 'POST':
+          await this.post(req, res);
+          break;
+        case 'PUT':
+          await this.put(req, res);
+          break;
+        case 'DELETE':
+          await this.delete(req, res);
+          break;
+        default:
+          response404(res);
+          break;
+      }
+    } catch (error) {
+      console.error(error);
+      response500(res);
+    }
+  }
+}
