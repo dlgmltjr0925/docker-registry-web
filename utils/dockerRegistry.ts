eb922f2ca5593b211655e567990c0e2569274412
fine <dlgmltjr0925@gmail.com>
2021-01-27 01:40:58 +0900

    태그 삭제 기능 구현

--- utils/dockerRegistry.ts
+++ utils/dockerRegistry.ts
@@ -42,7 +42,7 @@ export const getBase = async ({
 }: GetBaseArgs): Promise<{} | undefined> => {
   try {
     const configs: AxiosRequestConfig = {};
-    if (authorization) configs['headers'] = { Authorization: authorization };
+    if (authorization) configs.headers = { Authorization: authorization };
     const url = getRegistyUrl(host, '/');
     const res = await axios.get<{}>(url, configs);
     return res.data;
@@ -76,7 +76,7 @@ export const getImages = async ({
 }: GetImagesArgs): Promise<string[] | undefined> => {
   try {
     const configs: AxiosRequestConfig = {};
-    if (authorization) configs['headers'] = { Authorization: authorization };
+    if (authorization) configs.headers = { Authorization: authorization };
     let path = '/_catalog';
     if (n) path += `?n=${n}`;
     if (n && last !== undefined) path += `&last=${last}`;
@@ -119,9 +119,137 @@ export const getTags = async ({
 }: GetTagsArgs): Promise<GetTagsResponse | undefined> => {
   try {
     const configs: AxiosRequestConfig = {};
-    if (authorization) configs['headers'] = { Authorization: authorization };
+    if (authorization) configs.headers = { Authorization: authorization };
     const url = getRegistyUrl(host, `/${name}/tags/list`);
-    const res = await axios.get<{ name: string; tags: string[] }>(url, configs);
+    const res = await axios.get<GetTagsResponse>(url, configs);
+    return res.data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+/**
+ * GetTagsArgs
+ * host - docker registry hostname
+ * authorization - <scheme> <token> ex) Basic ZG9ja2VyOnJlZ2lzdHJ5
+ * name - Name of the target repository
+ * reference - Tag or digest of the target manifest
+ */
+interface GetManifestsArgs extends GetBaseArgs {
+  name: string;
+  reference: string;
+}
+
+interface FsLayer {
+  blobSum: string;
+}
+
+interface History {
+  v1Compatibility: string;
+}
+
+interface Signature {
+  header: {
+    jwk: {
+      crv: string;
+      kid: string;
+      kty: string;
+      x: string;
+      y: string;
+    };
+    alg: string;
+  };
+  signature: string;
+  protected: string;
+}
+
+interface Manifest {
+  name: string;
+  tag: string;
+  fsLayers: FsLayer[];
+  history: History[];
+  signatures: Signature[];
+}
+
+interface Layer {
+  mediaType: 'application/vnd.docker.image.rootfs.diff.tar.gzip';
+  size: number;
+  digest: string;
+}
+interface DistributionManifest {
+  schemaVersion: 2;
+  mediaType: 'application/vnd.docker.distribution.manifest.v2+json';
+  config: {
+    mediaType: 'application/vnd.docker.container.image.v1+json';
+    size: number;
+    digest: string;
+  };
+  layers: Layer[];
+}
+
+/**
+ * GetTagsResponse
+ */
+interface GetManifestsResponse extends Manifest {
+  digest: string;
+}
+
+/**
+ * GET /v2/<name>/manifests/<reference>
+ * Host: <registry host>
+ * Authorization: <scheme> <token>
+ */
+export const getManifests = async ({
+  host,
+  authorization,
+  name,
+  reference,
+}: GetManifestsArgs): Promise<GetManifestsResponse | undefined> => {
+  try {
+    const configs: AxiosRequestConfig = {};
+    if (authorization) configs.headers = { Authorization: authorization };
+    const url = getRegistyUrl(host, `/${name}/manifests/${reference}`);
+    const res1 = await axios.get<Manifest>(url, configs);
+    configs.headers.Accept =
+      'application/vnd.docker.distribution.manifest.v2+json';
+    const res2 = await axios.get<DistributionManifest>(url, configs);
+    const digest = res2.headers['docker-content-digest'] as string;
+    return { ...res1.data, digest };
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+/**
+ * DeleteTagsArgs
+ * host - docker registry hostname
+ * authorization - <scheme> <token> ex) Basic ZG9ja2VyOnJlZ2lzdHJ5
+ * name - Name of the target repository
+ * reference - Tag or digest of the target manifest
+ */
+type DeleteTagsArgs = GetManifestsArgs;
+
+/**
+ * DELETE /v2/<name>/manifests/<reference>
+ * Host: <registry host>
+ * Authorization: <scheme> <token>
+ */
+export const deleteManifests = async ({
+  host,
+  authorization,
+  name,
+  reference,
+}: DeleteTagsArgs): Promise<{} | undefined> => {
+  try {
+    let digest = reference;
+    if (!/^sha256/.test(digest)) {
+      const res = await getManifests({ host, authorization, name, reference });
+      if (res) digest = res.digest;
+    }
+    const configs: AxiosRequestConfig = {};
+    if (authorization) configs.headers = { Authorization: authorization };
+    const url = getRegistyUrl(host, `/${name}/manifests/${digest}`);
+    const res = await axios.delete<{}>(url, configs);
     return res.data;
   } catch (error) {
     handleError(error);
