e78c73ae014b7b877313b58dd63189e39598b50f
fine <dlgmltjr0925@gmail.com>
2021-03-03 18:27:20 +0900

    Fix prepare script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,8 +5,7 @@ data_dir_path='data'
 if ! test -d $data_dir_path; then
   echo '$data_dir_path : Not found the directory'
   mkdir $data_dir_path
+  cp ./scripts/prepare/docker-registry-ui.db ./data/
 fi
-
-cp ./scripts/prepare/docker-registry-ui.db ./data/
 
 node ./scripts/prepare/database.js
