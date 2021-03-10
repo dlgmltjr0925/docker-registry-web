d67daefd5f4f66ccb5217e88e9265870a7632660
fine <dlgmltjr0925@gmail.com>
2021-03-10 16:54:23 +0900

    Remove sqlite3 for arm64v8 build

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,7 +5,5 @@ data_dir_path='data'
 if ! test -d $data_dir_path; then
   echo '$data_dir_path : Not found the directory'
   mkdir $data_dir_path
-  cp ./scripts/prepare/docker-registry-ui.db ./data/
+  cp ./scripts/prepare/docker-registry-ui.json ./data/
 fi
-
-node ./scripts/prepare/database.js
